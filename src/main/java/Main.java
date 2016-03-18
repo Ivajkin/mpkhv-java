417647a6faef380bbadc47cc162b347487bdd203
Timofey Ivaykin <ivaykin@live.com>
2016-03-18 18:53:40 +1000

    GZip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,6 +25,11 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
+    after((request, response) -> {
+      response.header("Content-Encoding", "gzip");
+    });
+
+
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
