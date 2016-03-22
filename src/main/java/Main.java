fc920496f08a3687d878546ce0bd2949037de150
Timofey Ivaykin <ivaykin@live.com>
2016-03-22 16:49:38 +1000

    Freemarker ftl name error fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -66,7 +66,7 @@ public class Main {
       Map<String, Object> attributes = new HashMap<>();
       attributes.put("message", "Hello World!");
 
-      return new ModelAndView(attributes, "index.ftl");
+      return new ModelAndView(attributes, "mp-public-reception.ftl");
     }, new FreeMarkerEngine());
 
   }
